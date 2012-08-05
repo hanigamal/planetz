83d08ae9592daf59c8b1dc3821cf70d79f565731
mfawzy22@gmail.com <mfawzy22@gmail.com>
2012-08-06 00:22:09 +0200

    add autoload

---
+++ includes/class.autoload.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * class autoload files
+ * 
+ */ 
+class Autoload
+{
+	public function __construct()
+	{
+		$this->__autoload($classname);
+	}
+	
+	public function 	__autoload($classname)
+	{
+		$file = $classname. 'php';
+		if(file_exists($file))
+		{
+			require_once $file;
+		}
+		
+	}
+}

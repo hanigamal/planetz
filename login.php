ac56d864f6d3e35a7cfa84e38eeb3bec461a69f4
Hani Gamal <hanigamal@gmail.com>
2012-08-06 03:00:48 +0200

    TODO: Change this silent operator

--- login.php
+++ login.php
@@ -35,7 +35,7 @@
         <p><label for="username">Username:</label> <input type="text" name="username" value="<?PHP echo $username;?>" id="username" /></p>
         <p><label for="password">Password:</label> <input type="password" name="password" value="" id="password" /></p>
         <p><input type="submit" name="btnlogin" value="Login" id="btnlogin" /></p>
-        <input type="hidden" name="r" value="<?PHP echo htmlspecialchars(@$_REQUEST['r']); ?>" id="r">
+        <input type="hidden" name="r" value="<?PHP /* TODO: Change this silent operator */ echo htmlspecialchars(@$_REQUEST['r']); ?>" id="r">
     </form>
 </body>
 </html>
